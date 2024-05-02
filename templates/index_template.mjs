fcf717a319be408b1962264812f65ec0879287ad
collin <me@collin.sh>
2024-05-01 19:20:51 -0700

    add favicon

--- templates/index_template.mjs
+++ templates/index_template.mjs
@@ -8,9 +8,10 @@ export function index_template(data) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Luigi Galleani</title>
+    <title>${data.title}</title>
     <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
+    <link rel="icon" href="favicon.svg" type="image/svg+xml">
 </head>
 <body>
 <div class="container">
