8eea90d45f0c4745d92ea90f65916cef9845f106
collin <me@collin.sh>
2024-05-04 10:35:26 -0700

    improve mobile styling

--- wiki_app/node/index_template.mjs
+++ wiki_app/node/index_template.mjs
@@ -7,7 +7,7 @@ export function index_template(data) {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
     <title>${data.title}</title>
     <link rel="stylesheet" href="/static/style.css">
     <script src="/static/script.js" defer></script>
